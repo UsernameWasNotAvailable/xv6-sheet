6a16d159ce386395aa336873f3cc5939fa82c824
UsernameWasNotAvailable <99486800+UsernameWasNotAvailable@users.noreply.github.com>
2026-01-26 21:08:42 +0800

    Update input.c

--- input.c
+++ input.c
@@ -2,7 +2,7 @@ int main(int argc, char *argv[])
 {
   int i;
   for(i = 1; i < argc; i++){               // Loop through all arguments passed
-    
+    //your code here!!!
   }
   exit(0);                                 // Terminate the process
 }
