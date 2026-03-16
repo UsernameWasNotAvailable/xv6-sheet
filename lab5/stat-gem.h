66f49aa293eb58ac71c87396b238f3cb815b9f7e
UsernameWasNotAvailable <99486800+UsernameWasNotAvailable@users.noreply.github.com>
2026-03-17 01:04:06 +0800

    Create stat-gem.h

---
+++ lab5/stat-gem.h
@@ -0,0 +1,7 @@
+#define T_DIR     1   // Directory
+#define T_FILE    2   // File
+#define T_DEVICE  3   // Device
+// ---------------------------------------------------------
+// -> EXAM ADDITION HERE <-
+// ---------------------------------------------------------
+#define T_SYMLINK 4   // Symbolic Link
